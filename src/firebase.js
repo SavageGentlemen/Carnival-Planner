530cf7a0a6e4112cf67af3c7bcb6438de82ca47c
djkrss1 <djkrss1@gmail.com>
2025-12-03 01:07:41 +0000

    updatefbc

--- src/firebase.js
+++ src/firebase.js
@@ -14,9 +14,9 @@ import { getAnalytics, isSupported } from "firebase/analytics";
 // etc.
 const firebaseConfig = {
   apiKey: "AIzaSyCWp0X6bLJBsyYKTjUaVsXBSuVD8KeeEqY",
-  authDomain: "carnival-planner-49626964.firebaseapp.com",
-  projectId: "carnival-planner-49626964",
-  storageBucket: "carnival-planner-49626964.appspot.com",
+  authDomain: "carnival-planner.firebaseapp.com",
+  projectId: "carnival-planner",
+  storageBucket: "carnival-planner.firebasestorage.app",
   messagingSenderId: "1036340118282",
   appId: "1:1036340118282:web:809dc12c298ff1b8f2f0f3",
   measurementId: "G-XC1K69PSVC"
