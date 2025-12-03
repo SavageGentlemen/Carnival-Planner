8d679574ce0f870ca255baaabdcbcebb1f4ddb8a
djkrss1 <djkrss1@gmail.com>
2025-12-03 00:46:59 +0000

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,11 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCWp0X6bLJBsyYKTjUaVsXBSuVD8KeeEqY",
   authDomain: "carnival-planner.firebaseapp.com",
@@ -13,7 +16,6 @@ const firebaseConfig = {
   measurementId: "G-XC1K69PSVC"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const appId = "default-app-id";
+const analytics = getAnalytics(app);
